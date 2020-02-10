0774e520a715ccef73b92cf91a56fb6512dca5cd
KingdomCross <alexchheng1998@yahoo.com>
2020-02-09 19:39:19 -0800

    Initiate Continue

--- main.cpp
+++ main.cpp
@@ -2,19 +2,19 @@
 
 using namespace std;
 
-int Generate_Memory_Block(int Memory_Block_Table) {
-    int generate_memory_block = rand() % 100 + 25;
-    if (Memory_Block_Table >= generate_memory_block){
-        Memory_Block_Table -= generate_memory_block;
-    } else {
-        cout << "Out of memory space" << endl;
-        return Memory_Block_Table;
-    }
-    return Memory_Block_Table;
-}
+int * Initiate(int generate_memory_block) {
 
-int * PCB(){
-    int * PCB = rand();
+    //b and c
+    int * PCB;
+    PCB = new int[4];
+    PCB[0] = rand();
+
+    PCB[1] = 5; //default size, accomplish not related to assignment
+
+    int *PageTable;
+    PageTable = new int[generate_memory_block];
+
+    PCB[2] = *PageTable;
     return PCB;
 }
 
@@ -29,9 +29,26 @@ int main() {
         cout << "Select menu " << endl;
         cin >> user_input_number;
         if (user_input_number == 1){
-            Memory_Block_Table = Generate_Memory_Block(Memory_Block_Table);
+            //a, it need to return two value and function can only return one (Memory_Block_Table, generate_memory_block)
+            int generate_memory_block = rand() % (120 - 25 + 1) + 25; //high = 120, low = 25, http://www.cplusplus.com/forum/beginner/102036/
+            if (Memory_Block_Table >= generate_memory_block){
+                Memory_Block_Table -= generate_memory_block;
+            } else {
+                cout << "Out of memory space" << endl;
+                break;
+            }
+
+            //generate PCB
+            int * PCB = Initiate(generate_memory_block);
             cout << Memory_Block_Table << " Total Block remaining" << endl;
-            cout << PCB() << " PCB" << endl;
+            cout << PCB[0] << " " << PCB[1] << " " << PCB[2] << " PCB" << endl;
+        }
+
+        if (user_input_number == 2){
+            for (int n = 0; n < 10000; n++){
+                cout << rand() % (120 - 25 + 1) + 25 << endl;
+            }
+            cout << endl;
         }
 
         else {
